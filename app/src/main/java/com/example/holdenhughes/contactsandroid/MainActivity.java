54c10b04bed6319a45774e8c0a8f7993ccec78a4
holdenhughes <holdenmh121489@gmail.com>
2015-10-21 13:13:25 -0400

    Implemented add name and phone#

--- app/src/main/java/com/example/holdenhughes/contactsandroid/MainActivity.java
+++ app/src/main/java/com/example/holdenhughes/contactsandroid/MainActivity.java
@@ -2,12 +2,51 @@ package com.example.holdenhughes.contactsandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemLongClickListener {
+
+    ArrayAdapter<String> contacts;
+    ListView listView;
+    EditText nameText;
+    EditText phoneText;
+    Button addButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+        nameText = (EditText) findViewById(R.id.nameText);
+        phoneText = (EditText) findViewById(R.id.phoneText);
+        addButton = (Button) findViewById(R.id.addButton);
+
+        contacts = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(contacts);
+
+        addButton.setOnClickListener(this);
+        listView.setOnItemLongClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String text = nameText.getText().toString();
+        String text1 = phoneText.getText().toString();
+        String textFinal = String.format("%s (%s)",nameText,phoneText);
+        contacts.add(textFinal);
+        nameText.setText("Name");
+        phoneText.setText("Phone #");
+
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return false;
     }
 }
